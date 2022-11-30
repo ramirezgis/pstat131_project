5a79b3d58217515c0125b57516a879f5f2b0d116
ramirezgis <ramirezgis1225@gmail.com>
2022-11-30 01:49:12 -0800

    working on intro

--- Code/proj_code.R
+++ Code/proj_code.R
@@ -20,9 +20,24 @@ library(tidyr)
 library(janitor)
 tidymodels_prefer()
 
+#predicting cancellation of booking
+
+#hotel: h1 = resort hotel, h2 = city hotel
+#is_cancelled = not cancel (0); 1 = yes
+
 hotel_booking <- read.csv("hotel_bookings.csv")
 set.seed(1234)
 
 ### cleaning/preparing data/see missing data
 summary(hotel_booking)
 summarize_all(hotel_booking)
+
+#clean variable names
+hotel_booking <- hotel_booking %>%
+  clean_names()
+
+#unselect unimportant variables
+#country, agent, and company
+#deselect unimportant names
+hotelbooking <- hotel_booking %>%
+  select()
